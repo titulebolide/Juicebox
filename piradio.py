966b43c9699fc0ff291425569b77f014f7d964fc
titulebolide <seneztit@gmail.com>
2020-05-19 15:14:33 +0100

    intial commit

---
+++ piradio.py
@@ -0,0 +1,25 @@
+import subprocess
+import flask
+
+app = flask.Flask(__name__)
+
+p = None
+
+@app.route('/start/')
+def start():
+    global p
+    p = subprocess.Popen(['mplayer', 'http://streaming.radio.rtl.fr/rtl-1-44-128'])
+    return ""
+
+@app.route('/stop/')
+def stop():
+    global p
+    if p is not None:
+        p.kill()
+        p = None
+    return ""
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
+
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from piradio import app as application
+
+if __name__ == "__main__":
+    application.run()
+
