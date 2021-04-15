9b4eaa1e24adabed12adf540910fb4dbd33a1273
titulebolide <seneztit@gmail.com>
2021-04-15 10:22:04 +0200

    Add play/pause

--- piradio.py
+++ piradio.py
@@ -7,12 +7,37 @@ app = flask.Flask(__name__)
 radios = json.load(open('radios.json', 'r'))['radios']
 p = None
 selected_radio = 0
+stopped = False
 
 @app.route('/')
 def frontend():
-    global selected_radio
+    global selected_radio, stopped
     html = """<html><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head><body>"""
     html += """<div class='container'/>"""
+
+    if stopped:
+        btn_type='success'
+        href='/radio/'+str(selected_radio)
+        text='Play'
+    else:
+        btn_type='danger'
+        href='/stop'
+        text='Pause'
+
+    html += """
+<div class='row'>
+    <div class='col-sm-12'>
+        <center>
+            <a
+                class = "btn btn-outline-{}"
+                style = "width:50%;min-width:200px;margin-top:10px;"
+                href="{}">
+                {}
+            </a>
+        </center>
+    </div>
+</div>
+""".format(btn_type, href, text)
 
     for radio_id, radio in enumerate(radios):
         additionnal_style = ""
@@ -39,14 +64,25 @@ def frontend():
 
 @app.route('/radio/<radio_id>')
 def select_radio(radio_id):
-    global p, selected_radio
+    global p, selected_radio,stopped
+    stopped = False
     with open('default_radio.txt', 'w') as f:
         f.write(radio_id)
     radio_id = int(radio_id)
     selected_radio = radio_id
     if p is not None:
         p.kill()
+        p = None
     p = subprocess.Popen(['mplayer', radios[radio_id]['url']])
+    return frontend()
+
+@app.route('/stop/')
+def stop():
+    global p, selected_radio, stopped
+    if p is not None:
+        p.kill()
+        p = None
+    stopped = True
     return frontend()
 
 
@@ -61,7 +97,8 @@ if os.path.isfile('default_radio.txt'):
         selected_radio = 0
 else:
     selected_radio=0
-select_radio(str(selected_radio))
+if not stopped:
+    select_radio(str(selected_radio))
 
 
 
